099f12237f2802516ee1d268dce819918423b82d
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-10-07 10:50:08 -0400

    uncommneted App starter code in index.js

--- src/index.js
+++ src/index.js
@@ -16,13 +16,11 @@ import { ProfileListPage } from './components/pages/ProfileList';
 import { LoginPage } from './components/pages/Login';
 import { HomePage } from './components/pages/Home';
 import { ExampleDataViz } from './components/pages/ExampleDataViz';
-// import { config } from './utils/oktaConfig';
+import { config } from './utils/oktaConfig';
 import { LoadingComponent } from './components/common';
 import Search from 'antd/lib/input/Search';
 
 import SearchBar from './components/common/SearchBar.js';
-
-const App = () => <SearchBar />;
 
 ReactDOM.render(
   <Router>
@@ -33,33 +31,33 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// function App() {
-//   // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
-//   // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
-//   const history = useHistory();
+function App() {
+  // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
+  // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
+  const history = useHistory();
 
-//   // const authHandler = () => {
-//   //   // We pass this to our <Security /> component that wraps our routes.
-//   //   // It'll automatically check if userToken is available and push back to login if not :)
-//   //   history.push('/login');
-//   // };
+  const authHandler = () => {
+    // We pass this to our <Security /> component that wraps our routes.
+    // It'll automatically check if userToken is available and push back to login if not :)
+    history.push('/login');
+  };
 
-//   return (
-//     // <Security {...config} onAuthRequired={authHandler}>
-//       <Switch>
-//         <Route path="/login" component={LoginPage} />
-//         <Route path="/implicit/callback" component={LoginCallback} />
-//         {/* any of the routes you need secured should be registered as SecureRoutes */}
-//         <SecureRoute
-//           path="/"
-//           exact
-//           component={() => <HomePage LoadingComponent={LoadingComponent} />}
-//         />
-//         <SecureRoute path="/example-list" component={ExampleListPage} />
-//         <SecureRoute path="/profile-list" component={ProfileListPage} />
-//         <SecureRoute path="/datavis" component={ExampleDataViz} />
-//         <Route component={NotFoundPage} />
-//       </Switch>
-//     // </Security>
-//   );
-// }
+  return (
+    <Security {...config} onAuthRequired={authHandler}>
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/implicit/callback" component={LoginCallback} />
+        {/* any of the routes you need secured should be registered as SecureRoutes */}
+        <SecureRoute
+          path="/"
+          exact
+          component={() => <HomePage LoadingComponent={LoadingComponent} />}
+        />
+        <SecureRoute path="/example-list" component={ExampleListPage} />
+        <SecureRoute path="/profile-list" component={ProfileListPage} />
+        <SecureRoute path="/datavis" component={ExampleDataViz} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Security>
+  );
+}
