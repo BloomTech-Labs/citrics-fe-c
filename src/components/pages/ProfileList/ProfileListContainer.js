a9727db88f0379bef8d62af825858d7c0c69a154
Lauren Emick <laurenemick6@gmail.com>
2020-11-05 13:31:00 -0800

    comments out auth and adds hard coded profile info

--- src/components/pages/ProfileList/ProfileListContainer.js
+++ src/components/pages/ProfileList/ProfileListContainer.js
@@ -7,21 +7,51 @@ import { List } from '../../common';
 
 import RenderProfileListPage from './RenderProfileListPage';
 
+import { Canvas } from '../../layouts';
+
 // Here is an example of using our reusable List component to display some list data to the UI.
-const ProfileList = () => {
-  const { authState } = useOktaAuth();
+function ProfileList() {
+  // const { authState } = useOktaAuth();
 
   return (
-    <List
-      // Here we are passing our Axios request helper function as a callback.
-      getItemsData={() => getProfileData(authState)}
-      // Here we are passing in a component we want to show whilst waiting for our API request
-      // to complete.
-      LoadingComponent={() => <div>Loading Profiles...</div>}
-      // Here we are passing in a component that receives our new data and returns our JSX elements.
-      RenderItems={RenderProfileListPage}
-    />
-  );
-};
+    <div>
+      <section
+        style={{
+          marginTop: '50%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <div style={{ paddingRight: '6%' }}>
+          <h1 style={{ color: 'white', textAlign: 'center', padding: '4%' }}>
+            Lauren Emick
+          </h1>
+          <h1 style={{ color: 'white', textAlign: 'center' }}>Seattle, WA</h1>
+        </div>
+        <div>
+          <h3 style={{ color: 'white' }}>Username: laurenemick</h3>
+          <h3 style={{ color: 'white' }}>Email: lauren@lauren.com</h3>
+          <h3 style={{ color: 'white' }}>Phone: (123) 456-7890</h3>
+        </div>
+      </section>
+    </div>
 
-export default ProfileList;
+    // <div style={{marginTop: '10%'}}>
+    //   <h1 style={{color: 'white'}}>Hi</h1>
+    // </div>
+
+    // <List
+    //   // Here we are passing our Axios request helper function as a callback.
+    //   getItemsData={() => getProfileData(authState)}
+    //   // Here we are passing in a component we want to show whilst waiting for our API request
+    //   // to complete.
+    //   LoadingComponent={() => <div>Loading Profiles...</div>}
+    //   // Here we are passing in a component that receives our new data and returns our JSX elements.
+    //   RenderItems={RenderProfileListPage}
+    // />
+  );
+}
+
+// export default ProfileList;
+export default ({ styles }) => <Canvas Main={ProfileList} />;
